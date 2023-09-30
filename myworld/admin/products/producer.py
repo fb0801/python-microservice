0e36b59dc2eae7160f037ea69d23b50433e1b0f3
fb0801 <fbhatti0801@gmail.com>
2023-09-30 13:52:55 +0100

    queue service

--- myworld/admin/products/producer.py
+++ myworld/admin/products/producer.py
@@ -1,10 +1,12 @@
-import pika
+import pika, json
 
-params = pika.URLParameters('')
+params = pika.URLParameters('your_rabbitmq_url')
 
 connection = pika.BlockingConnection(params)
 
-channel  = connection.channel()
+channel = connection.channel()
 
-def publish():
-    channel.basic_publish(exchange='', routing_key='admin', body='hello')
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
